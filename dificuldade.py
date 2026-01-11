16b9c6c8b12139ab8029d7ed8ee726dde45e0bc7
Ninguem <arthurpellenz100@gmail.com>
2026-01-10 21:36:59 -0300

    bug fix + balanceamento

--- dificuldade.py
+++ dificuldade.py
@@ -3,15 +3,15 @@ class Dificuldade:
         self.set_dificuldade(nivel)
 
     def set_dificuldade(self, nivel):
-        self.nivel = nivel
+        self.nivel = nivel.lower()
         if nivel == 'fácil': #fácil e depois normal
-            self.mult_dano_jogador = 0.7 #dano NO jogador
-            self.mult_dano_inimigo = 1.4 #dano DO jogador
+            self.mult_dano_jogador = 0.8 #dano NO jogador
+            self.mult_dano_inimigo = 1.2 #dano DO jogador
             self.levas = 1
             print("facil")
         elif nivel == 'normal':
-            self.mult_dano_jogador = 0.8
-            self.mult_dano_inimigo = 1.1
+            self.mult_dano_jogador = 1
+            self.mult_dano_inimigo = 1
             self.levas = 1
             print("normal")
         elif nivel == 'criança da noite':
@@ -33,33 +33,34 @@ class Dificuldade:
     def chance(self, raridade):
         raridade = raridade.lower()
         chances = {
-            'Fácil': {
-                'comum': 85,  # Aumentado de 70
-                'incomum': 10,  # Reduzido de 20
-                'raro': 4,  # Reduzido de 7
-                'lendaria': 1  # Reduzido de 3
+            'fácil': {
+                'comum': 88,  # Aumentado de 70
+                'incomum': 8,  # Reduzido de 20
+                'raro': 3.5,  # Reduzido de 7
+                'lendaria': 0.5 # Reduzido de 3
             },
-            'Normal': {
+            'normal': {
                 'comum': 85,  # Aumentado de 60
                 'incomum': 10,  # Reduzido de 20
                 'raro': 4,  # Mantido igual
-                'lendaria': 2  # Reduzido de 13
+                'lendaria': 1  # Reduzido de 13
             },
-            'Criança Da Noite': {
-                'comum': 65,  # Aumentado de 50
-                'incomum': 15,  # Reduzido de 20
+            'criança da noite': {
+                'comum': 78,  # Aumentado de 50
+                'incomum': 10,  # Reduzido de 20
+                'raro': 8,  # Aumentado de 7
+                'lendaria': 4  # Reduzido de 23
+            },
+            'lua de sangue': {
+                'comum': 70,  # Aumentado de 40
+                'incomum': 10,  # Reduzido de 20
                 'raro': 10,  # Aumentado de 7
-                'lendaria': 10  # Reduzido de 23
-            },
-            'Lua De Sangue': {
-                'comum': 55,  # Aumentado de 40
-                'incomum': 15,  # Reduzido de 20
-                'raro': 15,  # Aumentado de 7
-                'lendaria': 15  # Reduzido de 33
+                'lendaria': 5  # Reduzido de 33
             }
         }
 
-        dist = chances.get(self.nivel, chances['Normal'])
+        dist = chances.get(self.nivel, chances['normal'])
+        print(dist.get(raridade, 0))
         return dist.get(raridade, 0)
 
 
--- inimigos/nuvemBoss2.py
+++ inimigos/nuvemBoss2.py
@@ -6,7 +6,7 @@ from inimigo import Inimigo
 from utils import resource_path 
 
 class NuvemBoss2(Inimigo):
-    def __init__(self, x, y, largura, altura, nome="Visão Ácida", hp=15000, velocidade=4, dano=85):
+    def __init__(self, x, y, largura, altura, nome="Visão Ácida", hp=10000, velocidade=4, dano=85):
         super().__init__(x, y, largura, altura, hp, velocidade, dano)
 
         self.nome = nome
